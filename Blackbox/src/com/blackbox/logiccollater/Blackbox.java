af5f6ad5bd62f2dc4f2d85a8cfa7c18f4dca9bc8
bhushannikhar1 <bhushannikhar1@users.noreply.github.com>
2018-11-10 23:10:10 +0530

    Initial project setup

---
+++ Blackbox/src/com/blackbox/logiccollater/Blackbox.java
@@ -0,0 +1,12 @@
+package com.blackbox.logiccollater;
+
+/*	This class is responsible for logic integrations
+ * 	@author : Bhushan Nikhar
+ * 
+ * */
+public class Blackbox {
+	public void loader()
+	{
+		System.out.println("Blackbox loader");
+	}
+}
